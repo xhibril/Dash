1cb9639daf6a73be699d91abe782b99f6e3f944a
Jistal <xhibril.dev@gmail.com>
2026-03-31 11:02:47 +0200

    fix route issue

--- Frontend/src/pages/dashboard/Dashboard.jsx
+++ Frontend/src/pages/dashboard/Dashboard.jsx
@@ -17,7 +17,7 @@ export default function Dashboard({ notify }) {
   const [selectedPeriod, setSelectedPeriod] = useState("DAILY");
   const [selectedUrl, setSelectedUrl] = useState("");
 
-  const domain = "dash.com/";
+  const domain = "api.xhibril.dev";
   const [urls, setUrls] = useState([]);
   const nav = useNavigate();
 
