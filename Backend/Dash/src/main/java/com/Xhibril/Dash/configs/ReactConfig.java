1e0a701aa81a1f01a0ea9afeb892bd72bea83ef9
Jistal <xhibril.dev@gmail.com>
2026-03-31 10:24:52 +0200

    fix backend nto accepting frontend requests

--- Backend/Dash/src/main/java/com/Xhibril/Dash/configs/ReactConfig.java
+++ Backend/Dash/src/main/java/com/Xhibril/Dash/configs/ReactConfig.java
@@ -15,7 +15,7 @@ public class ReactConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://dash.xhibril.dev"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
